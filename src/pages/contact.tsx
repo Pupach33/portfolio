1a42b67f61b18f9f63ea8d2a30d84fabb951030f
Pupach33 <114019737+Pupach33@users.noreply.github.com>
2023-05-05 11:54:15 +0300

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,7 +3,7 @@ export default function Contact(){
         <div className="about">
             
             <h1 className="h-m">.social &#123;</h1>
-            <h1 className="h">website: ;<br/></h1> 
+            <h1 className="h">website: <a className="link-cont" href="https://portfolio-88jj-590gjjnpm-pupach33.vercel.app/project">https://portfolio-88jj-590gjjnpm-pupach33.vercel.app/project</a>;<br/></h1> 
             <h1 className="h">email: <a className="link-cont" href="">fadvasa@gmail.com;</a> <br/></h1>
             <h1 className="h">github: <a  className="link-cont" href="https://github.com/Pupach33">https://github.com/Pupach33;</a><br/></h1> 
             <h1 className="h">telegram: <a  className="link-cont" href="t.me/Kukusik2" >Kukusik2;</a><br/></h1>
